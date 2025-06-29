2ef5b4efb1c8b7e4a555a483e56cd1af38cd557e
nyanshiba <31783332+nyanshiba@users.noreply.github.com>
2025-06-29 17:48:38 +0900

    traceroute計測に対応、任意のホップを選択可

--- atlas-exporter.ps1
+++ atlas-exporter.ps1
@@ -5,66 +5,96 @@ param(
     [switch]$Latest,
     [string]$Key = "974baae9-750a-427a-be78-e20797d6bbc4",
     [Int]$RateLimitSeconds = 86400,
-    [string]$LokiUrl = "http://loki.nuc.home.arpa:3100/loki/api/v1/push"
+    [string]$LokiUrl = "http://loki.home.arpa:3100/loki/api/v1/push",
+    [string[]]$HopLimits = (2, 5, 10)
 )
 
 Set-Location $PSScriptRoot
-$AddInterval = [Int]($RateLimitSeconds / (./asap.ps1 -Api /api/v2/measurements/$Measurement/ -Key $Key).interval)
+
+# 最後の計測だけ取得
 if ($Latest)
 {
-    $Result = ./asap.ps1 -Api /api/v2/measurements/$Measurement/latest/?probe_ids=$Probe -Key $Key
+    $Response = ./asap.ps1 -Api /api/v2/measurements/$Measurement/latest/?probe_ids=$Probe -Key $Key
+}
+# 全ての計測
+else
+{
+    $Response = ./asap.ps1 -Api /api/v2/measurements/$Measurement/results/?probe_ids=$Probe -Key $Key
+}
+
+# Lokiに送るラベルを定義
+$body = @{}
+$body.streams = New-Object System.Collections.ArrayList
+if ($Response[0].type -eq "traceroute")
+{
+    foreach ($h in $HopLimits)
+    {
+        $null = $body.streams.Add(@{
+            stream = @{
+                label = "atlas"
+                msm_id = "$Measurement"
+                prb_id = "$Probe"
+                hop = "$h"
+            }
+            values = New-Object System.Collections.ArrayList
+        })
+    }
 }
 else
 {
-    $Result = ./asap.ps1 -Api /api/v2/measurements/$Measurement/results/?probe_ids=$Probe -Key $Key
+    $null = $body.streams.Add(@{
+        stream = @{
+            label = "atlas"
+            msm_id = "$Measurement"
+            prb_id = "$Probe"
+        }
+        values = New-Object System.Collections.ArrayList
+    })
 }
 
-if ($Result[0].type -eq "ntp")
-{
-    $Factor = 1000
-}
-else
-{
-    $Factor = 1
-}
+# NTPは秒をミリ秒に直す
+$Factor = $Response[0].type -eq "ntp" ? 1000 : 1
 
+# Lokiに送る計測を選出する頻度
+$AddInterval = [Int]($RateLimitSeconds / (./asap.ps1 -Api /api/v2/measurements/$Measurement/ -Key $Key).interval)
+# 計測値を追加
 $cnt = $AddInterval
-$values = New-Object System.Collections.ArrayList
-$Result | ForEach-Object {
+$Response | ForEach-Object {
     if ($cnt -eq $AddInterval)
     {
-        $null = $values.Add(@(
-            [string]($_.timestamp * 1000000000),
-            [string]((($_.result.rtt | Sort-Object -Descending)[0] * $Factor))
-        ))
+        if ($_.type -eq "traceroute")
+        {
+            foreach ($h in $_.result.Where({$_.hop -In $HopLimits}))
+            {
+                $null = ($body.streams | Where-Object {$_.stream.hop -eq $h.hop}).values.Add(@(
+                    [string]($_.timestamp * 1000000000),
+                    [string]((($h.result.rtt | Sort-Object -Descending)[0] * $Factor))
+                ))
+            }
+        }
+        else
+        {
+            $null = $body.streams[0].values.Add(@(
+                [string]($_.timestamp * 1000000000),
+                [string]((($_.result.rtt | Sort-Object -Descending)[0] * $Factor))
+            ))
+        }
         $cnt = 0
     }
     $cnt++
 }
+
+$bodyJson = $body | ConvertTo-Json -Depth 5
+Write-Host $bodyJson
 
 if ($values.Count -gt 5000)
 {
     Write-Host "There are $($values.Count) values, but Grafana only supports a maximum of 5000."
     return 1
 }
+else
+{
+    Write-Host "There are $($values.Count) values"
+}
 
-$body =
-@{
-    streams =
-    @(
-        @{
-            stream =
-            @{
-                label = "atlas"
-                msm_id = "$Measurement"
-                prb_id = "$Probe"
-            }
-            values = $values
-        }
-    )
-} | ConvertTo-Json -Depth 5
-Write-Host $body
-
-Write-Host "There are $($values.Count) values"
-
-Invoke-RestMethod -Method Post -Uri $LokiUrl -ContentType "application/json" -Body $body
+Invoke-RestMethod -Method Post -Uri $LokiUrl -ContentType "application/json" -Body $bodyJson
